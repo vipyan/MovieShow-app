fc970dd7f22520dec33cd6062e5d8cf2f88e4f12
Vipin <vipinkaniyanthara@gmail.com>
2024-11-09 17:46:08 -0500

    added popular shows and spinner effect

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const global = {
 };
 
 //Fetch data from TMDB API and add to DOM
+//display 20 popular movies
 async function displayPopularMovies() {
     const { results }  = await fetchAPIData('movie/popular');
 
@@ -37,17 +38,62 @@ async function displayPopularMovies() {
 }
 
 
+//display 20 popular tv shows
+async function displayPopularShows() {
+    const { results }  = await fetchAPIData('tv/popular');
+
+    results.forEach((show) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `
+        
+          <a href="tv-details.html?id=${show.id}">
+            ${show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />` :
+                `<img
+              src="images/MovieShow.png"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+        document.querySelector('#popular-shows').appendChild(div);
+    });
+}
+
+
 async function fetchAPIData(endpoint) {
     const API_KEY = 'bd406a984ea7656e6fc591a0f3ec87a1';
     const API_URL = 'https://api.themoviedb.org/3/';
 
+    showSpinner();
+
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 
     const data = await response.json();
+   hideSpinner();
 
     return data;
 }
 
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
+}
 
 //Highlight Active link
 function highlightActiveLink() {
@@ -70,6 +116,7 @@ function init() {
             break;
         case '/shows.html':
             console.log('Shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
